10f4c803b7ca9f2f2f0c1dd5891b62a00f821c3d
LazyDotExe <papaporo@proton.me>
2023-07-29 14:37:02 -0500

    got post working

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,42 @@
 $(document).ready(function(){
     console.log('jQuery sourced.');
     // refreshBooks();
-    // addClickHandlers();
+    addClickHandlers();
   });
   
   function addClickHandlers() {
+    $('#submit').on('click', addToDo)
     
   
   
   
     // TODO - Add code for edit & delete buttons
   }
+
+
+  //------------------------------------------------------------------------------------------------------------------------------------------------------
+
+  //         POST
+  function addToDo() {
+    console.log('Add button clicked.');
+    let newToDo = {};
+    newToDo.task = $('#taskInput').val();
+    newToDo.date = $('#dateInput').val();
+    newToDo.description = $('#descriptionInput').val();
+    newToDo.completed = false;
+
+    $.ajax({
+        type: 'POST',
+        url: '/TODO',
+        data: newToDo,
+        }).then(function(response) {
+          console.log('Response from server.', response);
+        //   refreshList();
+        }).catch(function(error) {
+          console.log('Error in POST', error)
+          alert('Unable to add item at this time. Please try again later.');
+        });
+    
+  }
+  
+ 
--- server/routes/TODO.router.js
+++ server/routes/TODO.router.js
@@ -5,4 +5,26 @@ const pool = require('../modules/pool');
 
 
 
+
+
+
+
+router.post('/',  (req, res) => {
+    let newToDo = req.body;
+    console.log(`Adding book`, newToDo);
+  
+    let queryText = `INSERT INTO "TODO" ("task", "due_date", "Description", "completed")
+                     VALUES ($1, $2, $3, $4);`;
+    pool.query(queryText, [newToDo.task, newToDo.due_date, newToDo.description, newToDo.completed])
+      .then(result => {
+        res.sendStatus(201);
+      })
+      .catch(error => {
+        console.log(`Error adding new item`, error);
+        res.sendStatus(500);
+      });
+  });
+
+
+
 module.exports = router;
