81fc60bffe177e03735c32370e3db1852a73656d
LazyDotExe <papaporo@proton.me>
2023-07-29 13:23:25 -0500

    Setup initial server/route/html code

---
+++ server/public/scripts/client.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    console.log('jQuery sourced.');
+    // refreshBooks();
+    // addClickHandlers();
+  });
+  
+  function addClickHandlers() {
+    
+  
+  
+  
+    // TODO - Add code for edit & delete buttons
+  }
