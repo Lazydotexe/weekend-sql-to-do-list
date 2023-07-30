6e772520ea0d1f535eaa141531ae82bf6c229e97
LazyDotExe <papaporo@proton.me>
2023-07-30 00:59:40 -0500

    Added the table info to database.sql

--- database.sql
+++ database.sql
@@ -1,21 +1,22 @@
 CREATE TABLE "TODO" (
 	"id" SERIAL PRIMARY KEY,
 	"task" VARCHAR (250) NOT NULL,
-	"due_date" DATE,
 	"completed" BOOLEAN,
 	"Description" VARCHAR(180) NOT NULL
   
 );
 
-INSERT INTO "TODO" ("task", "due_date", "completed", "Description")
+INSERT INTO "TODO" ("task", "completed", "Description")
 VALUES 
-  ('Buy groceries', '2023-08-15', FALSE, 'Milk, eggs, bread, and vegetables'),
-  ('Pay electricity bill', '2023-08-15', FALSE, 'Use online banking to pay the electricity bill'),
-  ('Finish weekend project', '2023-07-30', FALSE, 'Make sure project is done by 6:00 pm'),
-  ('Feed tacocat', '2023-01-08', FALSE, 'He has not been fed in 20 years'),
-  ('Workout', '2023-08-03', FALSE, 'Lower body');
-
-
-
-
-    UPDATE "TODO" SET "completed" = TRUE WHERE "id" = 5;
+  ('Buy groceries', FALSE, 'Milk, eggs, bread, and vegetables'),
+  ('Pay electricity bill',  FALSE, 'Use online banking to pay the electricity bill'),
+  ('Finish weekend project',  FALSE, 'Make sure project is done by 6:00 pm'),
+  ('Feed tacocat', FALSE, 'He has not been fed in 20 years'),
+  ('Workout', FALSE, 'Lower body');
+  
+  
+  
+  UPDATE "TODO" SET "completed" = TRUE WHERE "id" = 5;
+  DELETE FROM "TODO" WHERE id = 7;
+  SELECT * FROM "TODO"
+  ORDER BY "task" DESC;
